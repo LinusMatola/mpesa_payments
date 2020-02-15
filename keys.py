b52f332126dcd33c3e2cb1997b7f0d87c2979756
Matola <matolawlinus@gmail.com>
2020-02-15 03:06:09 +0300

    authentication file

---
+++ keys.py
@@ -0,0 +1,9 @@
+businessShortCode = "174379"
+
+phone_number = "254722316454"
+
+lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+
+consumer_key = "1yfv9M2kygNXfwGXd6AirJtvbAHR4P9d"
+
+consumer_secret = "5g7abHAYXkUWhqG0"
